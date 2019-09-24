c1685d161360ae86bc36a2400a2ff4ab7aeeb344
Lee-Roy King <leeroywking@gmail.com>
2019-09-23 20:20:16 -0700

    Updates

---
+++ precache-manifest.d1ffb04d4448b3bb4ee4d9cb5afc5d20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479dae773a61ab1050360ab5d451dcc2",
+    "url": "/lab32/index.html"
+  },
+  {
+    "revision": "9ccf9adc609f0ccfc920",
+    "url": "/lab32/static/css/main.6389a10d.chunk.css"
+  },
+  {
+    "revision": "352f6bc6bccffd2dd120",
+    "url": "/lab32/static/js/2.6af8f237.chunk.js"
+  },
+  {
+    "revision": "9ccf9adc609f0ccfc920",
+    "url": "/lab32/static/js/main.9c909ef8.chunk.js"
+  },
+  {
+    "revision": "41221fc35c64b9042cff",
+    "url": "/lab32/static/js/runtime-main.e5550ed2.js"
+  }
+]);
